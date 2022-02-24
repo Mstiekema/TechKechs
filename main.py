3e71fb13b029e9cf3ae829308babbe9d832ab961
Merijn Stiekema <github@mstiekema.nl>
2022-02-24 19:01:26 +0100

    Parsing input for 2022 edition

--- main.py
+++ main.py
@@ -1,13 +1,42 @@
 import sys
+
 input = sys.stdin.readline
 
+# Global vars
 inputs = []
 
+# Function defs
+
+
+def check_person():
+    print("person")
+
+
+def check_project():
+    print("project")
+
+
+# Input
 while True:
     text = input().strip()
     if text:
         inputs.append(text)
     else:
         break
+
+# Runtime
+n_people, n_projects = inputs[0].split(" ")
+inputs.pop()
+
+n_people = int(n_people)
+n_projects = int(n_projects)
+
+while n_people > 0:
+    check_person()
+    n_people -= 1
+
+while n_projects > 0:
+    check_project()
+    n_projects -= 1
 
 print(inputs)
