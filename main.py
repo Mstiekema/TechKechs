08f30a1092615533c70c2a94b96ebdc0e4594675
Mstiekema <merijnstiekema@outlook.com>
2022-02-24 19:48:23 +0100

    Added base for projects and output

--- main.py
+++ main.py
@@ -5,23 +5,31 @@ input = sys.stdin.readline
 # Global vars
 inputs = []
 personDict = {}
-
-# Function defs
+result = {}
 
 
 def check_person():
     skillList = []
-    name, n_skills = inputs[0].split(" ") #get name and number of skills
+    name, n_skills = inputs[0].split(" ")  # get name and number of skills
     inputs.pop(0)
-    for i in range(int(n_skills)): #for each skill
-        skillList.append(inputs[0].split(" ")) #add it to the persons skill list
+    for i in range(int(n_skills)):  # for each skill
+        skillList.append(inputs[0].split(" "))  # add it to the persons skill list
         inputs.pop(0)
     personDict[name] = skillList
     print(name, personDict[name])
 
 
 def check_project():
-    print("project")
+    name, Di, Si, Bi, Ri = inputs[0].split(" ")
+    skills_required = int(Ri)
+    inputs.pop(0)
+
+    while skills_required > 0:
+        skill, level = inputs[0].split(" ")
+        skill_level = (skill, level)
+
+        inputs.pop(0)
+        skills_required -= 1
 
 
 # Input
@@ -43,8 +51,16 @@ while n_people > 0:
     check_person()
     n_people -= 1
 
+
 while n_projects > 0:
     check_project()
     n_projects -= 1
 
-print(inputs)
+
+# Print completed projects
+print(len(result))
+
+# Print each project and their participants
+for project, people in result:
+    print(project)
+    print(people.join(" "))
